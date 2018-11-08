bf54a7e1ae0058dcea83ad08a0d4fd5208c9ad91
rgllima <rglllima@gmail.com>
2018-11-08 00:26:27 -0300

    Cross-Origin Allowed

--- src/app.js
+++ src/app.js
@@ -13,8 +13,12 @@ app.use(bodyParser.json());
 const indexRoute = require('./routes/index-route');
 const XMLToJsonRoute = require('./routes/XMLToJson-Route');
 
-// var xmlParser = require('express-xml-bodyparser');
-// app.use(xmlParser());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+})
 
 app.use('/', indexRoute);
 app.use('/xml', XMLToJsonRoute);
