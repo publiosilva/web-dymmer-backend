2ddaa905514af0b6bcb709f7debf144d1b0c43bb
Rafael Gonçalves Lima <rafaellimataua@hotmail.com>
2019-01-21 10:18:15 -0300

    implementated multiple cyclic dependent features measure

---
+++ src/services/quality-measures/multipleCyclicDependentFeatures.js
@@ -0,0 +1,36 @@
+/**
+ * Multiple Cyclic Dependent Features (MCDF)
+ * MCDF = ∑(Number of participants in feature constraints and children of OR groups)
+ */
+
+exports.multipleCyclicDependentFeatures = async (featureModel) => {
+    let feature_tree = featureModel.feature_tree[0];
+    let constraints = featureModel.constraints;
+
+    let conKeys;
+    let conFeatures = [];
+
+    constraints.forEach(element => {
+        conKeys = Object.values(element)[0].replace(/[\s\W-]+/g, '').split('or');
+        conKeys.forEach(feature => {
+            conFeatures.push(feature)
+        })
+    });
+
+    return await countFeatureConstraintsChildrenOfORGroups(feature_tree, conFeatures);
+}
+
+async function countFeatureConstraintsChildrenOfORGroups (feature_tree, featConstList) {
+    let response = 0
+    if (feature_tree.children) {
+        for(let i=0; i<feature_tree.children.length; i++) {
+
+            featConstList.forEach(featureId => {
+                if (feature_tree.children[i].id === featureId && feature_tree.children[i].type === 'o')
+                    response += 1;
+            })
+            response += await countFeatureConstraintsChildrenOfORGroups(feature_tree.children[i], featConstList)
+        }
+    }
+    return response;
+}
