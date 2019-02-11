ea6046638758fdc6edfa6a337399a8114f860109
Rafael Gonçalves Lima <rafaellimataua@hotmail.com>
2019-02-11 15:56:06 -0300

    exclusion of duplicate items in the constraint vector

--- src/services/quality-measures/numberOfCrossTreeConstraints.js
+++ src/services/quality-measures/numberOfCrossTreeConstraints.js
@@ -2,17 +2,25 @@
  * Number of distinct features involved in cross-tree constraints (NFRI)
  */
 
-exports.numberOfCrossTreeConstraints = (featureModel) => {
-    let constraints = featureModel.constraints;
+exports.numberOfCrossTreeConstraints = featureModel => {
+  let constraints = featureModel.constraints;
 
-    let conKeys;
-    let conFeatures = [];
+  let conKeys;
+  let conFeatures = [];
 
-    constraints.forEach(element => {
-        conKeys = Object.values(element)[0].replace(/[\s\W-]+/g, '').split('or');
-        conKeys.forEach(feature => {
-            conFeatures.push(feature)
-        })
+  constraints.forEach(element => {
+    conKeys = Object.values(element)[0]
+      .replace(/[\s\W-]+/g, "")
+      .split("or");
+    conKeys.forEach(feature => {
+      conFeatures.push(feature);
     });
-    return conFeatures.length;
-}
+  });
+
+  // remove duplicate items
+  conFeatures = conFeatures.filter((elem, i) => {
+    return conFeatures.indexOf(elem) === i;
+  });
+
+  return conFeatures.length;
+};
