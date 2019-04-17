aae69ef9af25d153e11fa3330fcf7ad1ef0a25e1
Rafael Gonçalves Lima <rafaellimataua@hotmail.com>
2019-04-16 22:52:50 -0300

    insert-authorization-header

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const qualityMeasureRoutes = require('./routes/qualityMeasure');
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 })
