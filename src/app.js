0dfc7aacdb8ad48590eae5345bb669342947d0c0
Rafael Gonçalves Lima <rafaellimataua@hotmail.com>
2019-04-16 23:00:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const qualityMeasureRoutes = require('./routes/qualityMeasure');
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 })
 
