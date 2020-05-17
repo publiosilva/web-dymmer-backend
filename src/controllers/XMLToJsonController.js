67af6fc875375a596950be9c4ee50dde5b10b828
Publio Blenilio <publio.blenilio@gmail.com>
2020-05-17 15:00:05 -0300

    Update xml to json converter algorithm

--- src/controllers/XMLToJsonController.js
+++ src/controllers/XMLToJsonController.js
@@ -1,135 +1,182 @@
-const DOMParser = require('xmldom').DOMParser;
+const DOMParser = require("xmldom").DOMParser;
 
 exports.post = (req, res) => {
     let json = convertXMLToJson(req.body.xmlString);
     res.status(200).send(json);
-}
+};
 
 /* convert the XML document to json */
 convertXMLToJson = (data) => {
     let json = {};
-    let document = new DOMParser().parseFromString(data, 'text/xml');
+    let document = new DOMParser().parseFromString(data, "text/xml");
 
-    // name of feature model	
-    let featureModelName = document.getElementsByTagName('feature_model')[0].getAttribute('name');
-    json['name'] = featureModelName;
+    // name of feature model
+    let featureModelName = document
+        .getElementsByTagName("feature_model")[0]
+        .getAttribute("name");
+    json["name"] = featureModelName;
 
     // meta of feature model
-    let featureModelMeta = document.getElementsByTagName('data');
+    let featureModelMeta = document.getElementsByTagName("data");
     for (index = 0; index < featureModelMeta.length; index++) {
-        let dataName = featureModelMeta[index].getAttribute('name');
+        let dataName = featureModelMeta[index].getAttribute("name");
         let dataContent = featureModelMeta[index].textContent;
         json[dataName] = dataContent;
     }
 
     // tree
-    let treeString = document.getElementsByTagName('feature_tree')[0].textContent;
-    let tree = generateTree(treeString);
-    json['feature_tree'] = [tree];
+    let treeString = document.getElementsByTagName("feature_tree")[0]
+        .textContent;
+    let tree = generateTree(treeString, 1);
+    json["feature_tree"] = [tree];
 
     // constraints
-    let constraintsString = document.getElementsByTagName('constraints')[0].textContent;
+    let constraintsString = document.getElementsByTagName("constraints")[0]
+        .textContent;
     let constraints = generateConstraints(constraintsString);
-    json['constraints'] = constraints;
+    json["constraints"] = constraints;
 
     // contexts
-    let contextElements = document.getElementsByTagName('context');
+    let contextElements = document.getElementsByTagName("context");
     let contexts = generateContexts(contextElements);
-    json['contexts'] = contexts;
+    json["contexts"] = contexts;
 
     return JSON.stringify(json);
-}
+};
 
 /* creates individual tree nodes from a string */
 generateNode = (nodeString) => {
-    let id, type, name, multiplicity, children;
+    let id, type, name, multiplicity, children, nodeDepth;
 
     /* id */
-    id = nodeString.substring(nodeString.indexOf('(') + 1, nodeString.indexOf(')'));
+    id = nodeString.substring(
+        nodeString.indexOf("(") + 1,
+        nodeString.indexOf(")")
+    );
 
     /* type */
-    type = nodeString.substring(nodeString.indexOf(':') + 1, nodeString.indexOf(':') + 2);
+    type = nodeString.substring(
+        nodeString.indexOf(":") + 1,
+        nodeString.indexOf(":") + 2
+    );
 
-    if (type == ' ') {
-        type = '';
+    if (type == " ") {
+        type = "";
     }
 
     /* name */
-    if (type == '') {
-        name = nodeString.substring(nodeString.indexOf(':') + 2, nodeString.indexOf('('));
+    if (type == "") {
+        name = nodeString.substring(
+            nodeString.indexOf(":") + 2,
+            nodeString.indexOf("(")
+        );
     } else {
-        name = nodeString.substring(nodeString.indexOf(':') + 3, nodeString.indexOf('('));
+        name = nodeString.substring(
+            nodeString.indexOf(":") + 3,
+            nodeString.indexOf("(")
+        );
     }
 
     /* children */
     children = [];
 
     /* multiplicity */
-    if (type == 'g') {
-        multiplicity = nodeString.substring(nodeString.indexOf('[') + 1, nodeString.indexOf(']'));
+    if (type == "g") {
+        multiplicity = nodeString.substring(
+            nodeString.indexOf("[") + 1,
+            nodeString.indexOf("]")
+        );
     }
+
+    const depth = nodeString.match(/\t/g);
+    nodeDepth = depth ? depth.length : 0;
 
     /* return object */
-    if (type == 'g') {
-        return { id, type, multiplicity, name, children };
+    if (type == "g") {
+        return { id, type, multiplicity, name, children, nodeDepth };
     } else {
-        return { id, type, name, children };
+        return { id, type, name, children, nodeDepth };
     }
+};
+
+function getRootNode(treeString) {
+    const rows = treeString.split("\n");
+
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i] === "") rows.splice(i, 1);
+    }
+
+    return rows[0];
 }
 
-/* identifies the position of a node based on the id and inserts it */
-insertNode = (node, rootNode) => {
-    if (rootNode.id === node.id.substring(0, node.id.lastIndexOf('_'))) {
-        rootNode.children.push(node);
-    } else {
-        for (index in rootNode.children) {
-            if (node.id.indexOf(rootNode.children[index].id) != -1) {
-                insertNode(node, rootNode.children[index]);
+function organizeTreeNodes(treeNodes) {
+    const rootNode = treeNodes[0];
+
+    if (treeNodes.length > 1) {
+        let children = [];
+        let lastIndex = treeNodes.length - 1;
+
+        for (let i = lastIndex; i >= 1; i--) {
+            let node = treeNodes[i];
+
+            if (rootNode.nodeDepth + 1 === node.nodeDepth) {
+                children.push({ start: i, end: lastIndex + 1 });
+
+                lastIndex = i - 1;
             }
         }
-    }
-}
 
-/* creates the tree based on a string */
-generateTree = (treeString) => {
-    let treeArray = treeString.split('\n');
-    let rootNode = null;
+        while (children.length > 0) {
+            let element = children.pop();
 
-    for (index in treeArray) {
-        if (treeArray[index] !== '') {
-            let node = generateNode(treeArray[index]);
-
-            if (rootNode == null) {
-                rootNode = node;
-            } else {
-                insertNode(node, rootNode);
-            }
+            rootNode.children.push(
+                organizeTreeNodes(treeNodes.slice(element.start, element.end))
+            );
         }
     }
 
     return rootNode;
 }
 
+function generateTree(treeString) {
+    const treeStringRows = treeString.split("\n");
+    const treeNodes = [];
+
+    for (let i = 0; i < treeStringRows.length; i++) {
+        const row = treeStringRows[i];
+
+        if (row.trim() !== "") {
+            const node = generateNode(row);
+
+            treeNodes.push(node);
+        }
+    }
+
+    const rootNode = organizeTreeNodes(treeNodes);
+
+    return rootNode;
+}
+
 /* creates constraints based on a string */
 generateConstraints = (constraintsString) => {
-    let constraintsArray = constraintsString.split('\n');
+    let constraintsArray = constraintsString.split("\n");
     let constraints = [];
 
     for (index in constraintsArray) {
         let item = constraintsArray[index];
 
-        if (item !== '') {
+        if (item !== "") {
             let constraint = {};
-            let name = item.substring(0, item.indexOf(':'));
-            let value = item.substring(item.indexOf(':') + 1, item.length);
-            constraint['name'] = name;
-            constraint['value'] = value;
+            let name = item.substring(0, item.indexOf(":"));
+            let value = item.substring(item.indexOf(":") + 1, item.length);
+            constraint["name"] = name;
+            constraint["value"] = value;
             constraints.push(constraint);
         }
     }
 
     return constraints;
-}
+};
 
 /* create a set of contexts */
 generateContexts = (contextElements) => {
@@ -140,32 +187,38 @@ generateContexts = (contextElements) => {
     }
 
     return contexts;
-}
+};
 
 /* create a context */
 generateContext = (contextElement) => {
     let context = {
-        name: contextElement.getAttribute('name'),
+        name: contextElement.getAttribute("name"),
         resolutions: [],
         constraints: [],
-        isTheCurrent: false
+        isTheCurrent: false,
     };
 
     // resolutions
-    let resolutions = contextElement.getElementsByTagName('resolution');
+    let resolutions = contextElement.getElementsByTagName("resolution");
     for (let index = 0; index < resolutions.length; index++) {
         let resolution = {};
-        resolution['feature_id'] = resolutions[index].getAttribute('id');
-        resolution['feature_name'] = resolutions[index].getAttribute('feature');
-        resolution['status'] = resolutions[index].getAttribute('status') === 'true';
+        resolution["feature_id"] = resolutions[index].getAttribute("id");
+        resolution["feature_name"] = resolutions[index].getAttribute("feature");
+        resolution["status"] =
+            resolutions[index].getAttribute("status") === "true";
         context.resolutions.push(resolution);
     }
 
     // constraints
-    let contextConstraints = contextElement.getElementsByTagName('constraints');
-    if (contextConstraints.length !== 0 && contextConstraints[0].textContent !== "") {
-        context.constraints = generateConstraints(contextConstraints[0].textContent);
+    let contextConstraints = contextElement.getElementsByTagName("constraints");
+    if (
+        contextConstraints.length !== 0 &&
+        contextConstraints[0].textContent !== ""
+    ) {
+        context.constraints = generateConstraints(
+            contextConstraints[0].textContent
+        );
     }
 
     return context;
-}
+};
