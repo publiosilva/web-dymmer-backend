b269219fcd43c892215ba1e5b76181e1d73d04b9
Publio Blenilio <publio.blenilio@gmail.com>
2020-06-24 20:19:05 -0300

    feature: Add single hotspot features measure

---
+++ src/core/qualitymeasures/singleHotspotFeatures.js
@@ -0,0 +1,21 @@
+exports.execute = async (featureModel) => {
+    const [featureTree] = featureModel.feature_tree;
+
+    return singleHotspotFeatures();
+};
+
+function singleHotspotFeatures(featureTree) {
+    let counter = 0;
+
+    if (featureTree.type === "g" && featureTree.multiplicity === "1,1") {
+        counter += featureTree.children.length;
+    }
+
+    for (let i = 0; i < featureTree.children.length; i++) {
+        let node = featureTree.children[i];
+
+        counter += singleHotspotFeatures(node);
+    }
+
+    return counter;
+}
