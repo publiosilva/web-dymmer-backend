75ea51b831bac1c6c4eb8f70a22ccff799a0d021
Publio Blenilio <publio.blenilio@gmail.com>
2020-07-12 16:26:54 -0300

    Remove old files

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
--- src/util/constraintsUtils.js
+++
@@ -1,7 +0,0 @@
-function getConstraintVariables(constraint) {
-    const constraintStr = constraint.value;
-
-    return constraintStr.replace("~", "").split(" or ");
-}
-
-module.exports = { getConstraintVariables };
